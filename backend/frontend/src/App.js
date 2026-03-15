399d005d4311e1637e4290ce7727a1e8d7c7ab41
joshiiimayank <153385098+joshiiimayank@users.noreply.github.com>
2026-03-16 03:41:50 +0530

    Added frontend updates

---
+++ backend/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{textAlign:"center", padding:"40px"}}>
+      <h1>
+        Secure Cloud Storage with Blockchain‑Based Integrity Verification
+      </h1>
+
+      <p>Upload and Secure Your Files</p>
+    </div>
+  );
+}
+
+export default App;
